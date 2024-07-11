49293653af1a25b936e96cd7ce13d4e491ae8149
Cullen McDermott <cmcdermott@octo.ai>
2024-07-11 10:43:54 -0600

    add copilot chat

--- modules/home-manager/nvim/nvim/lua/plugins/astrocore.lua
+++ modules/home-manager/nvim/nvim/lua/plugins/astrocore.lua
@@ -58,7 +58,6 @@ return {
           end,
           desc = "Close buffer from tabline",
         },
-
         -- tables with just a `desc` key will be registered with which-key if it's installed
         -- this is useful for naming menus
         -- ["<Leader>b"] = { desc = "Buffers" },
