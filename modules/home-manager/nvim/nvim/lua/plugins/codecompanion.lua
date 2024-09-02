e475b63fc8a9cf6342e103b50f6b4d03ea3b5f4e
cullenmcdermott <cullenmcd@gmail.com>
2024-09-02 15:34:35 -0600

    feat: add codecompanion.nvim plugin configuration

---
+++ modules/home-manager/nvim/nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,35 @@
+return {
+  "olimorris/codecompanion.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-telescope/telescope.nvim", -- Optional
+    {
+      "stevearc/dressing.nvim", -- Optional: Improves the default Neovim UI
+      opts = {
+        log_level = "DEBUG",
+      },
+      adapters = {
+        anthropic = function()
+          return require("codecompanion.adapters").extend("anthropic", {
+            env = {
+              api_key = "cmd:op read op://Private/AnthropicAPIKey/credential",
+            },
+          })
+        end,
+      },
+      strategies = {
+        chat = {
+          adapter = "anthropic",
+        },
+        inline = {
+          adapter = "anthropic",
+        },
+        agent = {
+          adapter = "anthropic",
+        },
+      },
+    },
+  },
+  config = true,
+}
