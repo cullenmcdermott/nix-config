40ee83d594d175561ad84fb92cfb997fdb2478dc
cullenmcdermott <9535761+cullenmcdermott@users.noreply.github.com>
2026-04-08 12:06:53 -0600

    use flox 1.11.2

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
     darwin.url = "github:nix-darwin/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
 
-    flox.url = "github:flox/flox/v1.11.1";
+    flox.url = "github:flox/flox/v1.11.2";
 
     dagger.url = "github:dagger/nix";
     dagger.inputs.nixpkgs.follows = "nixpkgs";
