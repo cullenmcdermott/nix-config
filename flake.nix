bc5b201ebaf80406223ec998c0f2ee3887b3dc9e
cullenmcdermott <9535761+cullenmcdermott@users.noreply.github.com>
2026-04-24 23:14:15 -0600

    refactor(flake): adopt flake-parts skeleton, no behavior change

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
   description = "cullen's multi-platform nix configuration";
+
   nixConfig = {
     extra-substituters = [
       "https://cache.flox.dev"
@@ -10,8 +11,12 @@
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
@@ -36,144 +41,121 @@
   };
 
   outputs =
-    inputs@{
-      nixpkgs,
-      home-manager,
-      darwin,
-      flox,
-      dagger,
-      nix-homebrew,
-      mac-app-util,
-      flox-agentic,
-      ...
-    }:
-    let
-      mkDarwinConfig =
-        {
-          system,
-          username,
-          hostname,
-          claudeCodeOverrides ? { },
-          extraModules ? [ ],
-          extraHomeManagerModules ? [ ],
-        }:
+    inputs@{ flake-parts, nixpkgs, home-manager, darwin, flox, dagger, nix-homebrew, mac-app-util, flox-agentic, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "aarch64-darwin" "x86_64-linux" ];
+
+      flake =
         let
-          baseConfiguration =
-            { pkgs, ... }:
+          mkDarwinConfig =
             {
-              imports = [ ./modules/common ];
-
-            };
-
-          homeManagerConfiguration = {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              backupFileExtension = "back";
-              extraSpecialArgs = {
-                inherit inputs username claudeCodeOverrides;
+              system,
+              username,
+              hostname,
+              claudeCodeOverrides ? { },
+              extraModules ? [ ],
+              extraHomeManagerModules ? [ ],
+            }:
+            let
+              baseConfiguration = { pkgs, ... }: {
+                imports = [ ./modules/common ];
               };
-              users.${username}.imports = [
-                ./modules/home-manager
-                mac-app-util.homeManagerModules.default
+
+              homeManagerConfiguration = {
+                home-manager = {
+                  useGlobalPkgs = true;
+                  useUserPackages = true;
+                  backupFileExtension = "back";
+                  extraSpecialArgs = {
+                    inherit inputs username claudeCodeOverrides;
+                  };
+                  users.${username}.imports = [
+                    ./modules/home-manager
+                    mac-app-util.homeManagerModules.default
+                  ]
+                  ++ extraHomeManagerModules;
+                };
+              };
+            in
+            darwin.lib.darwinSystem {
+              specialArgs = {
+                inherit username inputs;
+              };
+              modules = [
+                {
+                  nixpkgs.hostPlatform = system;
+                  nixpkgs.config.allowUnfree = true;
+                }
+                baseConfiguration
+                homeManagerConfiguration
+                home-manager.darwinModules.home-manager
+                nix-homebrew.darwinModules.nix-homebrew
+                mac-app-util.darwinModules.default
+                {
+                  nix-homebrew = {
+                    enable = true;
+                    enableRosetta = true;
+                    user = username;
+                    mutableTaps = true;
+                  };
+                }
+                ./modules/darwin
               ]
-              ++ extraHomeManagerModules;
+              ++ extraModules;
+            };
+
+          mkDistroboxEnvConfig =
+            {
+              system,
+              username,
+              homeDirectory,
+              flakeRef,
+              claudeCodeOverrides ? { },
+              extraModules ? [ ],
+              extraPackages ? [ ],
+            }:
+            home-manager.lib.homeManagerConfiguration {
+              pkgs = import nixpkgs {
+                inherit system;
+                config.allowUnfree = true;
+              };
+              extraSpecialArgs = {
+                inherit inputs username flakeRef claudeCodeOverrides;
+              };
+              modules = [
+                ./modules/home-manager
+                ./modules/home-manager/distrobox.nix
+                {
+                  home.username = username;
+                  home.homeDirectory = homeDirectory;
+                  home.packages = extraPackages;
+                }
+              ]
+              ++ extraModules;
+            };
+        in
+        {
+          darwinConfigurations = {
+            "cullens-MacBook-Pro" = mkDarwinConfig {
+              username = "cullen";
+              system = "aarch64-darwin";
+              hostname = "cullens-MacBook-Pro";
+              extraModules = [ ./systems/personal/default.nix ];
             };
           };
-        in
-        darwin.lib.darwinSystem {
-          specialArgs = {
-            inherit username inputs;
+
+          lib = {
+            inherit mkDarwinConfig mkDistroboxEnvConfig;
           };
-          modules = [
-            {
-              nixpkgs.hostPlatform = system;
-              nixpkgs.config.allowUnfree = true;
-            }
-            baseConfiguration
-            homeManagerConfiguration
-            home-manager.darwinModules.home-manager
-            nix-homebrew.darwinModules.nix-homebrew
-            mac-app-util.darwinModules.default
-            {
-              nix-homebrew = {
-                enable = true;
-                enableRosetta = true;
-                user = username;
-                mutableTaps = true;
-              };
-            }
-            ./modules/darwin
-          ]
-          ++ extraModules;
-        };
 
-      mkDistroboxEnvConfig =
-        {
-          system,
-          username,
-          homeDirectory,
-          flakeRef,
-          claudeCodeOverrides ? { },
-          extraModules ? [ ],
-          extraPackages ? [ ],
-        }:
-        home-manager.lib.homeManagerConfiguration {
-          pkgs = import nixpkgs {
-            inherit system;
-            config.allowUnfree = true;
+          homeConfigurations = {
+            "cullen@distrobox" = mkDistroboxEnvConfig {
+              system = "x86_64-linux";
+              username = "cullen";
+              homeDirectory = "/home/cullen";
+              flakeRef = "github:cullenmcdermott/nix-config";
+            };
           };
-          extraSpecialArgs = {
-            inherit
-              inputs
-              username
-              flakeRef
-              claudeCodeOverrides
-              ;
-          };
-          modules = [
-            ./modules/home-manager
-            ./modules/home-manager/distrobox.nix
-            {
-              home.username = username;
-              home.homeDirectory = homeDirectory;
-              home.packages = extraPackages;
-            }
-          ]
-          ++ extraModules;
         };
-
-    in
-    {
-      darwinConfigurations = {
-        "cullens-MacBook-Pro" = mkDarwinConfig {
-          username = "cullen";
-          system = "aarch64-darwin";
-          hostname = "cullens-MacBook-Pro";
-          extraModules = [ ./systems/personal/default.nix ];
-        };
-
-        # Example: Work laptop with Claude Code overrides
-        # "work-macbook" = mkDarwinConfig {
-        #   username = "cullen";
-        #   system = "aarch64-darwin";
-        #   hostname = "work-macbook";
-        #   extraHomeManagerModules = [ ./systems/work/claude-code.nix ];
-        # };
-      };
-
-      lib = {
-        inherit mkDarwinConfig mkDistroboxEnvConfig;
-      };
-
-      homeConfigurations = {
-        "cullen@distrobox" = mkDistroboxEnvConfig {
-          system = "x86_64-linux";
-          username = "cullen";
-          homeDirectory = "/home/cullen";
-          flakeRef = "github:cullenmcdermott/nix-config";
-        };
-      };
-
     };
 }
